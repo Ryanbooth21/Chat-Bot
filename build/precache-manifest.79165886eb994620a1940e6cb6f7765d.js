ad90bccc3ddfb5b4504ae6763f2c82e88db48ba4
Ryanbooth21 <boothrc@dukes.jmu.edu>
2019-12-21 06:54:50 -0500

    create build and change title

---
+++ build/precache-manifest.79165886eb994620a1940e6cb6f7765d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce3b0808c111aa0dda79acfab8242cd",
+    "url": "/Chat-Bot/index.html"
+  },
+  {
+    "revision": "7f50216345d67a7a22e2",
+    "url": "/Chat-Bot/static/css/main.e9c58d13.chunk.css"
+  },
+  {
+    "revision": "bcb25edba61dff487c1f",
+    "url": "/Chat-Bot/static/js/2.ef4b512f.chunk.js"
+  },
+  {
+    "revision": "7f50216345d67a7a22e2",
+    "url": "/Chat-Bot/static/js/main.56e3f9b4.chunk.js"
+  },
+  {
+    "revision": "1ec8dacdcdc14351bef7",
+    "url": "/Chat-Bot/static/js/runtime-main.47b8d137.js"
+  }
+]);
